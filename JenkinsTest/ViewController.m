6afc0e36a6fccebb61d0c0746b5afcee9bd2a48e
Jingliang Wen <wenhappy456@gmail.com>
2016-06-04 12:40:18 +0800

    init

---
+++ JenkinsTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JenkinsTest
+//
+//  Created by 温敬亮 on 16/6/4.
+//  Copyright © 2016年 lanyou. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
